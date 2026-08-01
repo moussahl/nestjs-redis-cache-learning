1745b4882573f31220d733d4068e10b1f0334640
Moussa Haloui <hliimoussa2020@gmail.com>
2026-08-01 19:09:08 +0100

    feat: add redis cacheModule globally

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service.js';
 import { ProductModule } from './product/product.module.js';
 import { PrismaService } from '../prisma.service.js';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [ConfigModule.forRoot(),ProductModule],
+  imports: [ConfigModule.forRoot(),ProductModule,
+    CacheModule.register({
+      ttl: 60, // seconds
+      isGlobal: true,})
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
