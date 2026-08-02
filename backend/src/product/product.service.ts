e57e5e98b760bd88c1f1662872c646443c0262b7
Moussa Haloui <hliimoussa2020@gmail.com>
2026-08-02 01:05:06 +0100

    feat: add Redis caching to product service

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,42 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
 import { Prisma, Product } from '../generated/prisma/client';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class ProductService {
+  constructor(
+    private prisma: PrismaService,
 
-    constructor(private prisma: PrismaService){}
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
+  ) {}
 
-    async getProducts(){
-        return await this.prisma.product.findMany()
+  //Get all products
+  async getProducts() {
+    const cacheKey = 'products:all';
+
+    // Check Redis
+    const cachedProducts = await this.cacheManager.get(cacheKey);
+
+    if (cachedProducts) {
+      console.log('FRPOM REDIS');
+      return cachedProducts;
     }
 
-    async getProduct(id: number){
-        return await this.prisma.product.findUnique({
-            where:{
-                id: Number(id)
-            }
-        })
+    //Get from Database
+    const products = await this.prisma.product.findMany();
+
+    // Save in Redis
+    await this.cacheManager.set(cacheKey, products);
+
+    console.log('FROM DATABASE');
+
+    return products;
+  }
+
+  async getProduct(id: number) {
+    const cacheKey = `product:${id}`;
+
+    //verify Redis
+    const cahcedProduct = await this.cacheManager.get(cacheKey);
+
+    if (cahcedProduct) {
+      return cahcedProduct;
     }
 
+    // Get from DATABASE
+    const product = await this.prisma.product.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
 
-    async createProduct(data: Prisma.ProductCreateInput): Promise<Product>{
-        return  await this.prisma.product.create({
-            data
-        })
+    //Save in Redis
+    if (product) {
+      await this.cacheManager.set(cacheKey, product);
     }
 
+    return product;
+  }
 
+  // Create Prodcut
+  async createProduct(data: Prisma.ProductCreateInput): Promise<Product> {
+    // Save DATABSE
+    const product = this.prisma.product.create({
+      data,
+    });
 
+    // Invalidate cache
 
+    const cacheKey = `products:all`;
+    await this.cacheManager.del(cacheKey);
 
-
-
-
-
-
-
-
-
-
+    return product;
+  }
 }
