17951a4db5b35047a4adc6a939886785cd4c6122
Moussa Haloui <hliimoussa2020@gmail.com>
2026-08-02 22:19:52 +0100

    feat(validation): add validation rules to CreateProductDto

--- backend/src/product/dto/create-product.dto.ts
+++ backend/src/product/dto/create-product.dto.ts
@@ -1,8 +1,12 @@
-
-
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 
 export class CreateProductDto {
+    @IsNotEmpty()
+    @IsString()
     name!: string;
 
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(0)
     price!: number;
 }
