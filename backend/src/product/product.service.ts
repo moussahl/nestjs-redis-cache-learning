79fa01eedf3f23ff8a80fc166dc7c4f21cd166d2
Moussa Haloui <hliimoussa2020@gmail.com>
2026-08-02 21:49:36 +0100

    feat(product): add update and delete operations with DTO support

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,8 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
 import { Prisma, Product } from '../generated/prisma/client';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update.product.dto';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class ProductService {
@@ -27,6 +30,10 @@ export class ProductService {
 
     //Get from Database
     const products = await this.prisma.product.findMany();
+
+    if(!products){
+      throw new NotFoundException(`n`)
+    }
 
     // Save in Redis
     await this.cacheManager.set(cacheKey, products);
@@ -62,16 +69,52 @@ export class ProductService {
   }
 
   // Create Prodcut
-  async createProduct(data: Prisma.ProductCreateInput): Promise<Product> {
+  async createProduct(dto: CreateProductDto): Promise<Product> {
     // Save DATABSE
     const product = this.prisma.product.create({
-      data,
+      data: {
+        ...dto,
+      },
     });
 
     // Invalidate cache
 
     const cacheKey = `products:all`;
     await this.cacheManager.del(cacheKey);
+
+    return product;
+  }
+
+  // Update
+  async updateProduct(id: number, dto: UpdateProductDto): Promise<Product> {
+    const product = await this.prisma.product.update({
+      where: {
+        id,
+      },
+      data: {
+        ...dto,
+      },
+    });
+
+    //invalidate cache
+    await this.cacheManager.del('products:all');
+    await this.cacheManager.del(`product:${id}`);
+
+    return product;
+  }
+
+  //Delete Product
+
+  async deleteProduct(id: number): Promise<Product> {
+    const product = await this.prisma.product.delete({
+      where: {
+        id,
+      },
+    });
+
+    // invalidate cache
+    await this.cacheManager.del('products:all');
+    await this.cacheManager.del(`product:${id}`);
 
     return product;
   }
