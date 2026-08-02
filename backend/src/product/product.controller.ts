fb86b06f3f50b8cae38647b261e6366c8fd32a78
Moussa Haloui <hliimoussa2020@gmail.com>
2026-08-02 21:51:07 +0100

    feat(product): add update and delete API endpoints

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,30 +1,43 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ProductService } from './product.service.js';
 import { CreateProductDto } from './dto/create-product.dto.js';
+import { UpdateProductDto } from './dto/update.product.dto.js';
 
 @Controller('products')
 export class ProductController {
+  constructor(private productService: ProductService) {}
 
-constructor(private productService: ProductService){}
+  @Get()
+  getProducts() {
+    return this.productService.getProducts();
+  }
 
+  @Get(':id')
+  getProduct(@Param('id') id: string) {
+    return this.productService.getProduct(Number(id));
+  }
 
-@Get()
-getProducts(){
+  @Post()
+  createProduct(@Body() data: CreateProductDto) {
+    return this.productService.createProduct(data);
+  }
 
-    return this.productService.getProducts()
+  @Patch(':id')
+  updateProduct(@Param('id') id: string, @Body() data: UpdateProductDto) {
+    return this.productService.updateProduct(Number(id), data);
+  }
 
-}
-
-@Get(':id')
-getProduct(@Param('id') id:string ){
-   return this.productService.getProduct(Number(id));
-}
-
-@Post()
-createProduct(@Body() data:CreateProductDto){
-    return this.productService.createProduct(data)
-}
-
-
-
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productService.deleteProduct(Number(id));
+  }
 }
