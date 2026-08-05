dfdad0f4227356cb65b168b833d689eb07e413bc
Moussa Haloui <hliimoussa2020@gmail.com>
2026-08-05 18:26:59 +0100

    feat: add global response interceptor

---
+++ backend/src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,20 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { map, Observable } from "rxjs";
+
+
+
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor{
+
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
+        return next.handle().pipe(
+            map((data)=> {
+                return {
+                    success: true,
+                    data,
+                }
+            })
+        )
+    }
+}
