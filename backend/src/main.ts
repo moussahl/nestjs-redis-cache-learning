fa7709729f0a8353ff593f27ae0993eafc56dc54
Moussa Haloui <hliimoussa2020@gmail.com>
2026-08-05 18:19:07 +0100

    feat: add global HTTP exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-   app.useGlobalPipes(
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
